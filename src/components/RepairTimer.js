1a2ddd81721ad76df14d6be23c7b6a67ed983587
José Ángel Anguita Rovira <jaanguita@MBP-de-Jose.home>
2022-03-15 07:09:37 +0100

    Repair time to 30mins

--- src/components/RepairTimer.js
+++ src/components/RepairTimer.js
@@ -6,13 +6,13 @@ import '../resources/css/RepairTimer.scss';
 
 function RepairTimer({onTimeFiasco, onRepairTimerChange}) {
     const { t } = useTranslation();
-//1800000
+
     return <div className="repairTimeContainer">
             <TimerControl 
                 label={t('points.reparacion')}
                 onTimeFiasco={onTimeFiasco}
                 onTimerChange={onRepairTimerChange}
-                maxTime={3000} />
+                maxTime={1800000} />
         </div>;
 }
 
