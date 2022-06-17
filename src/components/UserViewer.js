6aa439897d2248f2652a73f9599129099bb57f73
neuroflip <jaanguita@gmail.com>
2022-06-17 21:58:25 +0200

    More info at the public user profile (#64)
    * More info at the public user profile
    Co-authored-by: n <n@WIN-O3BMD4HSKNH.home>

--- src/components/UserViewer.js
+++ src/components/UserViewer.js
@@ -8,22 +8,33 @@ function UserViewer({uid}) {
     const { t } = useTranslation();
     const firebase = window.crawlear.fb;
     const [user, setUser] = React.useState({});
+    const [userData, setUserData] = React.useState({});
 
     React.useEffect(()=>{
-//        if(window.crawlear && window.crawlear.user && window.crawlear.user.uid) {
-            firebase.getUser(uid, (user)=>{
-                setUser({...user});
+        firebase.getUser(uid, (user)=>{
+            setUser({...user});
+            firebase.getUserExtraData(uid, (data)=>{
+                setUserData(data);
             });
-//        }
+        });
     }, []);
 
     if (user.registrationDate ) {
         return <div className="userViewer">
             <UserProfile user={user} />
+            <div>
+                {t('description.partidascreadas')}: {userData.ownerGames || 0}
+            </div>
+            <div>
+                {t('description.partidasdejuez')}: {userData.judgeGames || 0}
+            </div>
+            <div>
+                {t('description.partidasprevias')}: {userData.pilotGames || 0}
+            </div>
         </div>;
     } else {
         return <div className=''>
-                <img src={logo} className="userViewerLogo" alt="web logo"></img>
+                <a href="https://crawlear.com" target="_blank"><img src={logo} className="userViewerLogo" alt="web logo"></img></a>
                 <p>{t('content.userprofilenotlogged')}</p>
             </div>;
     }
