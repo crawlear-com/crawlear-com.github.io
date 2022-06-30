b03ea5747e049539efd3d755c515c1d14121d56f
José Ángel Anguita Rovira <jaanguita@MacBook-Pro-de-Jose.local>
2022-06-30 14:36:01 +0200

    Simplify lateral menu

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -3,8 +3,6 @@ import { useTranslation } from 'react-i18next';
 
 import logo from '../resources/img/logo5.png'
 import '../resources/css/Menu.scss';
-import pdfAECAR from '../resources/pdf/Reglamento Campeonato Nacional de Crawler 2022.pdf'
-import pdfISRCC from '../resources/pdf/Reglamento ISRCC.pdf'
 
 const LIGHTMODE_CLASS = 'lightMode';
 
@@ -39,17 +37,14 @@ function Menu() {
                 <div className="burguerMenuBar"></div>
                 <div className="linksContainer">
                     <ul>
-                        {userIsLoged ? <li><a href="/completegame">{t("description.herramientajuego")}</a></li> : <></>}
-                        {userIsLoged ? <li><a href={`/profile?uid=${window.crawlear.user.uid}`}>{t("description.perfilsocial")}</a></li> : <></>}
-                        {userIsLoged ? <li>-</li> : <></>}
-                        <li><a href="/">{t("description.paginaprincipal")}</a></li>
+                        <li><a href="/completegame">{t("description.herramientajuego")}</a></li>
+                        <li><a href={`/profile?uid=${window.crawlear.user.uid}`}>{t("description.perfilsocial")}</a></li>
+                        <li>-</li>
                         <li><a href="/privacypolicy">{t("description.politicaprivacidad")}</a></li>
                         <li><a href="/aboutus">{t("description.aboutus")}</a></li>
                         <li>-</li>
                         <li><a href="https://www.aecar.org/modalidades.php?tipo=crawler">AECAR Crawler</a></li>
-                        <li><a href={pdfAECAR}>Reglamento Campeonato Nacional de Crawler 2022</a></li>
                         <li><a href="https://isrcc.eu/">ISRCC International Scale Rock Crawler Championship</a></li>
-                        <li><a href={pdfISRCC}>Reglamento ISRCC 2022</a></li>
                     </ul>
                     <a className="lightModeSwitch" href="#clear" onClick={switchLightMode}>Modo claro / oscuro</a>
                 </div>
