4db87c24d758f6585e27fca07edc175db45896d3
n <n@ns-Mac-mini.home>
2022-07-12 00:14:13 +0200

    login status on needed routes

--- src/components/routes/MainContainer.js
+++ src/components/routes/MainContainer.js
@@ -3,14 +3,17 @@ import ViewNavigator from '../ViewNavigator';
 import UserViewer from './UserViewer';
 import FeedViewer from '../FeedViewer';
 import Footer from '../Footer';
+import { UserStatusContext } from '../context/UserStatusContext';
 
 function MainContainer({onLogin, onLogout}) {
     const col1Ref = React.useRef();
     const col2Ref = React.useRef();
     const col3Ref = React.useRef();
     const uid = window.crawlear && window.crawlear.user && window.crawlear.user.uid;
+    const { isUserLoged } = React.useContext(UserStatusContext);
 
-    return <ViewNavigator col1={col1Ref} col2={col2Ref}>
+    if (isUserLoged){
+        return <ViewNavigator col1={col1Ref} col2={col2Ref}>
         <div id='col1' ref={col1Ref} className='col1 current animated'>
             <UserViewer onLogin={()=>{onLogin(false)}} onLogout={onLogout} uid={uid}/>
             <Footer></Footer>
@@ -20,6 +23,11 @@ function MainContainer({onLogin, onLogout}) {
             <Footer></Footer>
         </div>
     </ViewNavigator>;
+
+    } else {
+        return <>Not logged! go to <a href='https://crawlear.com'>crawlear.com</a></>
+    }
+
 }
 
 export default MainContainer;
