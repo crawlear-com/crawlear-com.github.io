4654aba229579790bfb62db744498803de197d4e
neuroflip <jaanguita@gmail.com>
2022-09-28 13:11:42 +0200

    Added winch to the mini crawler passion game type (#96)
    Co-authored-by: José Ángel Anguita Rovira <jaanguita@MacBook-Pro-de-Jose.local>

--- src/components/games/MiniCrawlerPassionGameScores.js
+++ src/components/games/MiniCrawlerPassionGameScores.js
@@ -1,17 +1,59 @@
+import * as React from 'react';
 import Analytics from '../../Analytics';
-import { Levante124GameScores as MiniCrawlerPassionGameScores,
-    getGameContent, gameExtras } from './Levante124GameScores';
+import { gameExtras } from './Levante124GameScores';
+import AecarPoints from './AecarPoints';
+import ControlTextArray from '../ControlTextArray';
 
-MiniCrawlerPassionGameScores.texts = ['points.puerta',
-'points.tocarcoche',
-'points.saltoobstaculo',
-'points.vuelco',
-'points.saltopelota',
-'points.reparacion'
-];
+const MiniCrawlerPassionGameScores = {
+    steps: [1, 3, 5, 5, 5, 5, 10],
+    maxValues: [0, 0, 0, 0, 0, 0, 0],
+    texts: ['points.puerta',
+        'points.tocarcoche',
+        'points.saltoobstaculo',
+        'points.vuelco',
+        'points.saltopelota',
+        'points.winch',
+        'points.reparacion'
+    ],
+    fiascoTexts: ['points.bateria',
+        'points.reparacion'],
+    fiascoSteps: [1,1],
+    fiascoMaxValues: [1,1],
+    courtesyTime: 0,
+    maxPoints: 80,
+    maxTime: 480000
+};
 
-MiniCrawlerPassionGameScores.maxPoints = 80;
-MiniCrawlerPassionGameScores.maxTime = 480000;
+function getGameContent(t, player, zone, points) {
+    const childrenContent = [];
+
+    childrenContent.push(<ControlTextArray
+        controlTextValuesString='controlTextValues'
+        textToken={'description.penalizaciones'}
+        player={player}
+        zone={zone}
+        steps={MiniCrawlerPassionGameScores.steps}
+        maxValues={MiniCrawlerPassionGameScores.maxValues}
+        texts={MiniCrawlerPassionGameScores.texts}
+        isClosed={false} />);
+
+    childrenContent.push(<ControlTextArray
+        textToken={'points.fiascos'}
+        controlTextValuesString='fiascoControlTextValues'
+        steps={MiniCrawlerPassionGameScores.fiascoSteps}
+        maxValues={MiniCrawlerPassionGameScores.fiascoMaxValues}
+        texts={MiniCrawlerPassionGameScores.fiascoTexts}
+        player={player}
+        zone={zone}
+        isClosed={true}
+    />);
+
+    childrenContent.push(<AecarPoints 
+            player={player}
+            zone={zone} />);
+
+    return childrenContent;
+}
 
 gameExtras.doPageView = ()=> {
     Analytics.pageview('/minicrawlerpassion/');
