a24bb9e0e4e086b074e73baf9630ceb5922f2fb1
José Ángel Anguita Rovira <jaanguita@MBP-de-Jose.home>
2022-12-14 10:23:24 +0100

    Service worker: added analytics, and image cache first

--- src/pwa/initPwa.js
+++ src/pwa/initPwa.js
@@ -1,17 +1,20 @@
+import Analytics from "../Analytics";
 
 function initPwa() {
     if ('serviceWorker' in navigator) {
         window.addEventListener('load', function() {
         navigator.serviceWorker.register('service-worker.js').then(function(registration) {
-              console.log('Registered!');
-          }, function(err) {
-              console.log('ServiceWorker registration failed: ', err);
-          }).catch(function(err) {
-              console.log(err);
-          });
+            Analytics.event('App','pwa','registered');
+            }, function(err) {
+                Analytics.event('App','pwa','registration error');
+                console.log('ServiceWorker registration failed: ', err);
+            }).catch(function(err) {
+                console.log(err);
+            });
         });
       } else {
-          console.log('service worker is not supported');
+            Analytics.event('App','pwa','service worker not supported');
+            console.log('service worker is not supported');
       }
 }
 
--- src/pwa/service-worker.js
+++ src/pwa/service-worker.js
@@ -1,15 +1,34 @@
+import Analytics from "../Analytics";
+
+const CACHE_NAME = "crawlearCache_v1";
 
 //eslint-disable-next-line
 self.addEventListener('install', function() {
-    console.log('Install!');
+    Analytics.event('App','pwa','install');
 });
 
 //eslint-disable-next-line
 self.addEventListener("activate", event => {
-    console.log('Activate!');
+    Analytics.event('App','pwa','activate');
 });
 
 //eslint-disable-next-line
 self.addEventListener('fetch', function(event) {
+  if (event.request.destination === 'image') {
+    event.respondWith(caches.open(CACHE_NAME).then((cache) => {
+      return cache.match(event.request.url).then((cachedResponse) => {
+        if (cachedResponse) {
+            
+            return cachedResponse;
+        }
+        return fetch(event.request).then((fetchedResponse) => {
+            cache.put(event.request, fetchedResponse.clone());
 
+            return fetchedResponse;
+        });
+      });
+    }));
+  } else {
+    return;
+  }
 });
