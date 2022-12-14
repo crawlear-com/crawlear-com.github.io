138071066970a165e15e1b0aed102d96c93aac19
neuroflip <jaanguita@gmail.com>
2022-12-14 10:35:28 +0100

    Service worker: added analytics, and image cache first (#105)
    * Service worker: added analytics, and image cache first
    Co-authored-by: José Ángel Anguita Rovira <jaanguita@MBP-de-Jose.home>

--- src/pwa/service-worker.js
+++ src/pwa/service-worker.js
@@ -1,15 +1,32 @@
+const CACHE_NAME = "crawlearCache_v1";
 
 //eslint-disable-next-line
 self.addEventListener('install', function() {
-    console.log('Install!');
+    console.log(`CrawlearServiceWorker::${CACHE_NAME} Installed`);
 });
 
 //eslint-disable-next-line
 self.addEventListener("activate", event => {
-    console.log('Activate!');
+    console.log(`CrawlearServiceWorker::${CACHE_NAME} Activated`);
 });
 
 //eslint-disable-next-line
 self.addEventListener('fetch', function(event) {
+  if (event.request.destination === 'image') {
+    event.respondWith(caches.open(CACHE_NAME).then((cache) => {
+      return cache.match(event.request.url).then((cachedResponse) => {
+        if (cachedResponse) {
+            
+            return cachedResponse;
+        }
+        return fetch(event.request).then((fetchedResponse) => {
+            cache.put(event.request, fetchedResponse.clone());
 
+            return fetchedResponse;
+        });
+      });
+    }));
+  } else {
+    return;
+  }
 });
