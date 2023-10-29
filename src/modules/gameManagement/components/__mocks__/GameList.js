dbb9f4427431290d07eb07073c785ddf1ca39aa6
neuroflip <neuroflip@gmail.com>
2023-10-29 20:40:50 +0100

    Corrected GameList tests

--- src/modules/gameManagement/components/__mocks__/GameList.js
+++
@@ -1,5 +0,0 @@
-const GameList = ({games, gameProgressions, readOnly, onRemoveGame, onConfigureGame, title, onGamePlay}) => {
-    return <div title="GameList">GameList</div>
-}
-
-export default GameList
---
+++ src/modules/gameManagement/components/__mocks__/GameList.tsx
@@ -0,0 +1,8 @@
+import React from "react"
+import { GameListProps } from "../GameList"
+
+const GameList = ({ games, readOnly, onRemoveGame, onConfigureGame, title, onGamePlay }: GameListProps) => {
+    return <div title="GameList">GameList</div>
+}
+
+export default GameList
