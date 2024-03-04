d64f3b5faeb20d2997072d995780377557267eb4
neuroflip <jaanguita@gmail.com>
2024-03-04 18:01:51 +0100

    Language modularized to be lazy loaded, first step with Landing to improve page speed (#182)
    Co-authored-by: neuroflip <neuroflip@gmail.com>

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -9,14 +9,14 @@ function Footer() {
     const firebase = window.crawlear.fb;
 
     return <div className="Footer">
-        [©<a href="https://crawlear.com">crawlear.com</a> 2023
+        [©<a href="https://crawlear.com">crawlear.com</a> 2024
         {firebase.isUserLogged() ? <>]</> : <>
             - <a href="https://crawlear.com/aboutus">{t('description.aboutus')}</a> - <a href="https://crawlear.com/privacypolicy">{t('description.politicaprivacidad')}</a>]</>}
 
     <p>
-        <a href="https://www.youtube.com/channel/UCNwhl3Mflf-hpTSI1Xo8dSg" target="_blank"><img className='youtubeLogo' src={youtubeLogo} alt="youtube logo" /></a>
-        <a href="https://www.instagram.com/crawlearcom/" target="_blank"><img className='instagramLogo' src={instagramLogo} alt="instagram logo" /></a>
-        <a href="https://www.facebook.com/Crawlearcom-106005795418134" target="_blank"><img className='facebookLogo' src={facebookLogo} alt="facebook logo" /></a>
+        <a href="https://www.youtube.com/channel/UCNwhl3Mflf-hpTSI1Xo8dSg" target="_blank"><img width={35} height={35} className='youtubeLogo' src={youtubeLogo} alt="youtube logo" /></a>
+        <a href="https://www.instagram.com/crawlearcom/" target="_blank"><img width={35} height={35} className='instagramLogo' src={instagramLogo} alt="instagram logo" /></a>
+        <a href="https://www.facebook.com/Crawlearcom-106005795418134" target="_blank"><img width={35} height={35} className='facebookLogo' src={facebookLogo} alt="facebook logo" /></a>
     </p>
 
     </div>;
---
+++ src/resources/language/i18n.ts
@@ -0,0 +1,22 @@
+import i18n, { InitOptions } from 'i18next'
+import { initReactI18next } from 'react-i18next'
+import LanguageDetector from 'i18next-browser-languagedetector'
+import LazyImportPlugin from './LazyImport'
+
+
+i18n.use(LanguageDetector)
+  .use(LazyImportPlugin)
+  .use(initReactI18next)
+  .init({
+    fallbackLng: 'en',
+    interpolation: {
+      escapeValue: false,
+    },
+    ns: ['main'],
+    saveMissing: true, // for missing key handler to fire
+    missingKeyHandler: function (lng, ns, key, fallbackValue) {
+      console.log(key);
+    }
+  })
+
+export default i18n
