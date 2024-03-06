9335bced8af2c0a9d35f6cc0182bec4891d4244f
neuroflip <jaanguita@gmail.com>
2024-03-06 02:09:59 +0100

    Removed mutable elements in install cache (#187)
    * Removed mutable elements in install cache
    
    ---------
    
    Co-authored-by: neuroflip <neuroflip@gmail.com>

--- src/pwa/service-worker-dev.js
+++ src/pwa/service-worker-dev.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "crawlearCacheDEV_v3";
+const CACHE_NAME = "crawlearCacheDEV_v4";
 
 //eslint-disable-next-line
 self.addEventListener('install', event => {
@@ -8,16 +8,7 @@ self.addEventListener('install', event => {
         caches.open(CACHE_NAME).then(function(cache) {
           return cache.addAll([
             './',
-            'index.html',
-            '/main.js',
-            '/88eb99d0c3739d8fccb7.png',
-            '/b8cd4b8a8d6dc6abfb82.png',
-            '/630f3e1e30bbbf36a209.jpeg',
-            '/0e8411388912e8090839.png',
-            '/616535897b5598e00b94.png',
-            '/c02f061a017d59f5bb86.svg',
-            '/b0c484b364dc559b8644.svg',
-            '/5aa9dd97f0c1a5039ca6.svg'
+            'index.html'
           ]);
         })
       );
