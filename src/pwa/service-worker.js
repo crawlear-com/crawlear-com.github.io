28080436600ace6770b5e6435ffd42b1a5a77900
neuroflip <neuroflip@gmail.com>
2024-03-06 02:09:27 +0100

    Removed mutable elements in install cache

--- src/pwa/service-worker.js
+++ src/pwa/service-worker.js
@@ -24,15 +24,14 @@ self.addEventListener('fetch', function(event) {
     if (event.request.destination === 'image') {
         event.respondWith(caches.open(CACHE_NAME).then((cache) => {
             return cache.match(event.request.url).then((cachedResponse) => {
-            if (cachedResponse) {
-                
-                return cachedResponse
-            }
-            return fetch(event.request).then((fetchedResponse) => {
-                cache.put(event.request, fetchedResponse.clone())
-    
-                return fetchedResponse
-            })
+                if (cachedResponse) {
+                    return cachedResponse
+                }
+
+                return fetch(event.request).then((fetchedResponse) => {
+                    cache.put(event.request, fetchedResponse.clone())
+                    return fetchedResponse
+                })
             })
         }))
     } else {
