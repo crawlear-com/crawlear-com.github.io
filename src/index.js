dbfddf394e6ca6601fef336bdbc9dfc278ab5c2b
neuroflip <jaanguita@MacBook-Pro-de-Jose.local>
2024-04-04 17:41:19 +0200

    Added react-snap to SSG

--- src/index.js
+++ src/index.js
@@ -1,16 +1,21 @@
 import React from 'react';
-import { createRoot } from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
 import initPwa from './pwa/initPwa';
 import i18n from './resources/language/i18n';
+import { hydrateRoot, createRoot } from 'react-dom/client';
 
-const root = createRoot(document.getElementById('root'));
-root.render(
-  <BrowserRouter>
-      <App />
-  </BrowserRouter>);
+const appElement = <BrowserRouter>
+                      <App />
+                   </BrowserRouter>
+
+const container = document.getElementById('root')
+const hasChildNodes = container?.hasChildNodes() ?? false
+
+hasChildNodes
+  ? hydrateRoot(container, appElement)
+  : createRoot(container).render(appElement);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
