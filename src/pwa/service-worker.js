59aad2fe53a863646d1aaeb6406ed51d0396538b
neuroflip <jaanguita@gmail.com>
2024-04-12 11:52:10 +0200

    New production SW with version control to remove previous old cache when update to avoid update errors from cache (#202)
    Co-authored-by: neuroflip <jaanguita@MacBook-Pro-de-Jose.local>

--- src/pwa/service-worker.js
+++ src/pwa/service-worker.js
@@ -1,6 +1,19 @@
 import { precacheAndRoute, createHandlerBoundToURL } from "workbox-precaching"
 import { registerRoute, NavigationRoute } from "workbox-routing"
-import { NetworkFirst, StaleWhileRevalidate} from "workbox-strategies"
+import { StaleWhileRevalidate } from "workbox-strategies"
+import { setCacheNameDetails } from 'workbox-core'
+
+const CACHE_NAME = 'crawlearcom'
+const CACHE_VERSION = '1.0.1'
+const PREVIOUS_CACHE_VERSION = '1.0.0'
+const PREVIOUS_CACHE_NAMES = `${CACHE_NAME}-${CACHE_NAME}-precache-${PREVIOUS_CACHE_VERSION}`
+
+setCacheNameDetails({
+  prefix: CACHE_NAME,
+  suffix: CACHE_VERSION,
+  precache: `${CACHE_NAME}-precache`,
+  runtime: `${CACHE_NAME}-run-time`
+})
 
 // Precarga la app
 // eslint-disable-next-line no-restricted-globals
@@ -14,3 +27,19 @@ registerRoute(/^http?._/, new StaleWhileRevalidate(), "GET")
 const handler = createHandlerBoundToURL('/')
 const navigationRoute = new NavigationRoute(handler)
 registerRoute(navigationRoute)
+
+//eslint-disable-next-line
+self.addEventListener("activate", event => {
+  console.log(`CrawlearServiceWorker::${CACHE_NAME} Activated`);
+  event.waitUntil(
+    caches.keys().then(function(cacheNames) {
+      return Promise.all(
+        cacheNames.filter(function(cacheName) {
+          return PREVIOUS_CACHE_NAMES === cacheName
+        }).map(function(cacheName) {
+          return caches.delete(cacheName);
+        })
+      );
+    })
+  );
+});
