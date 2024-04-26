0b9acc8ae9a1a0f55bfd355e70a00244e0e90c89
neuroflip <jaanguita@gmail.com>
2024-04-26 11:00:00 +0200

    Added screenshots in static to be able to reference them from the web (#212)
    Co-authored-by: neuroflip <jaanguita@MacBook-Pro-de-Jose.local>

--- src/pwa/service-worker.js
+++ src/pwa/service-worker.js
@@ -4,8 +4,8 @@ import { StaleWhileRevalidate } from "workbox-strategies"
 import { setCacheNameDetails } from 'workbox-core'
 
 const CACHE_NAME = 'crawlearcom'
-const CACHE_VERSION = '1.0.14'
-const PREVIOUS_CACHE_VERSION = '1.0.13'
+const CACHE_VERSION = '1.0.16'
+const PREVIOUS_CACHE_VERSION = '1.0.15'
 const PREVIOUS_CACHE_NAMES = `${CACHE_NAME}-${CACHE_NAME}-precache-${PREVIOUS_CACHE_VERSION}`
 
 setCacheNameDetails({
