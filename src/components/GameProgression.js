73670f3bbff428515ec3a271474ea0b1a41d516a
neuroflip <jaanguita@MacBook-Pro-de-Jose.local>
2024-09-26 11:37:19 +0200

    Encapsulated some functionality from GameProgression into a custom Hook

--- src/components/GameProgression.js
+++ src/components/GameProgression.js
@@ -2,7 +2,7 @@ import * as React from 'react'
 import { useTranslation } from 'react-i18next'
 import GameProgressionInfoRow from './GameProgressionInfoRow'
 import { GameUtils } from '../games/Game'
-import { GameProgressionContext } from '../context/GameProgressionContext'
+import UseGameProgression from './hooks/UseGameProgression'
 
 const STATUS_WAITING = 'waiting'
 const STATUS_PLAYING = 'playing'
@@ -11,71 +11,11 @@ const STATUS_DONE = 'done'
 
 function GameProgression({game, jidGroup, onZoneClick}) {
     const { t } = useTranslation(['main']);
-    const [selectedZone, setSelectedZone] = React.useState(-1);
-    const [selectedPlayer, setSelectedPlayer] = React.useState(-1);
+    const [getNotAvailableZones, prepareOnClick, resolveGameStatus,
+        gameProgression, selectedPlayer, selectedZone] = UseGameProgression(onZoneClick, t, game)
     const gameProgressionInfoRef = React.useRef();
-    const [ gameProgression, setGameProgression ] = React.useContext(GameProgressionContext);
     const playersDone = [];
     let i=0;
-
-    function prepareOnClick(event, player) {
-        const zone = Number(event.target.closest('[data-zone]').getAttribute("data-zone"));
-        const gameStatus = gameProgression[player.group][player.id][zone].status;
-
-        if (selectedZone === zone && selectedPlayer === player.id) {
-            deselectPlayerAndZone();
-        } else {
-            if(gameStatus === STATUS_WAITING || gameStatus === STATUS_DONE || gameStatus === STATUS_PLAYING) {
-                setSelectedPlayer(player.id);
-                setSelectedZone(zone);
-                onZoneClick(player, zone);
-            } else {
-                deselectPlayerAndZone();
-            }
-        }
-    }
-
-    function deselectPlayerAndZone() {
-        onZoneClick(-1, -1);
-        setSelectedPlayer(-1);
-        setSelectedZone(-1);
-    }
-
-    function renderOccupiedZones(zones) {
-        const result = [];
-
-        zones.forEach((zone, zIndex)=>{
-            const row = [];
-            
-            row.push(<span key={zIndex}>{t('description.zona')} {zIndex+1}: </span>);
-            if (zone) {
-                row.push(<span key={`dG${zIndex}`} className='directorGroup colorRed'>{t('description.ocupado')}</span>);
-            } else {
-                row.push(<span key={`div${zIndex}`}><span className='directorGroup'>{t('description.libre')}</span><br /></span>);
-            }
-            result.push(<div key={`dZ${zIndex}`} className='directorZone'>{row}</div>);
-        });
-
-        return <div className=''>
-            {result}
-            </div>;
-    }
-
-    function getNotAvailableZones() {
-        const zones = new Array(game.zones).fill(false);
-
-        gameProgression && Object.entries(gameProgression).forEach((group, gIndex)=> {
-            Object.entries(group[1]).forEach((player, pIndex)=>{
-                Object.entries(player[1]).forEach((zone, zIndex)=>{
-                    if(zone[1].status === 'playing') {
-                        !zones[zIndex] && (zones[zIndex] = true);
-                    }
-                });
-            });
-        });
-
-        return renderOccupiedZones(zones);
-    }
 
     playersDone.push(<div key="eZ" className=''>{t('content.estadozona')}</div>);
     playersDone.push(<div key="nAz">{getNotAvailableZones()}</div>);
@@ -136,34 +76,6 @@ function GameProgression({game, jidGroup, onZoneClick}) {
     });
 
     return <>{ playersDone }</>
-}
-
-function resolveGameStatus(t, status, data) {
-    let result;
-
-    switch(status) {
-        case 'waiting':
-            if (data) {
-                result = t('description.reparacionfinalizada');
-            } else {
-                result = t('description.waiting');
-            }
-            break;
-        case 'done':
-            result = t('description.done');
-            break;
-        case 'repair':
-            result = t('description.repair');
-            break;
-        case 'playing':
-            result = t('description.playing');
-            break;
-        default:
-            result = t('description.waiting');
-            break;
-    }
-
-    return result;
 }
 
 export default GameProgression;
