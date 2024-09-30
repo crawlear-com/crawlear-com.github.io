fe0afe64813b44dce37ec311025cf79782b7a093
neuroflip <jaanguita@gmail.com>
2024-09-30 12:04:16 +0200

    Little refactor in Menu and his tests (#232)
    Co-authored-by: neuroflip <jaanguita@MacBook-Pro-de-Jose.local>

--- src/tests/components/Menu.test.js
+++ src/tests/components/Menu.test.js
@@ -1,4 +1,4 @@
-import { render, act } from '@testing-library/react';
+import { render, screen, act } from '@testing-library/react';
 import { BrowserRouter } from 'react-router-dom';
 import Menu from '../../components/Menu.js';
 
@@ -28,45 +28,48 @@ afterEach(() => {
 })
 
 test('renders Menu closed', () => {
-  const { container } = render(<BrowserRouter><Menu /></BrowserRouter>, div),
-    menu = container.querySelector('.menuContainer'),
-    menuEntries = menu.querySelector(".linksContainer");
+  render(<BrowserRouter><Menu /></BrowserRouter>, div)
+  
+  const menu = screen.getByTestId('menuContainer')
+  const menuEntries = screen.queryByTestId("linksContainer");
+  const menuBars = screen.getAllByTestId("burguerMenuBar")
 
-  expect(menu.querySelectorAll(".burguerMenuBar").length).toBe(3);
+  expect(menuBars.length).toBe(3);
   expect(menu.classList.contains("closed")).toBeTruthy();
   expect(menuEntries).toBeNull();
 });
 
 test('opens Menu', () => {
-    const { container } = render(<BrowserRouter><Menu /></BrowserRouter>, div),
-      menu = container.querySelector('.menuContainer');
-  
-    expect(menu.classList.contains("open")).toBeFalsy();
-    act(() => {
-      menu.click()
-    })
-  
-    expect(menu.classList.contains("open")).toBeTruthy();
-  });
-
-  test('renders Menu open', () => {
-    const { container } = render(<BrowserRouter><Menu /></BrowserRouter>, div),
-      menu = container.querySelector('.menuContainer');
+    render(<BrowserRouter><Menu /></BrowserRouter>, div)
+    
+    const menu = screen.getByTestId('menuContainer')
   
     expect(menu.classList.contains("open")).toBeFalsy();
     act(() => {
       menu.click();
     })
-    const links = menu.querySelectorAll(".linksContainer li");
+    expect(menu.classList.contains("open")).toBeTruthy();
+  });
+
+  test('renders Menu open', () => {
+    render(<BrowserRouter><Menu /></BrowserRouter>, div)
+    const menu = screen.getByTestId('menuContainer')
+  
+    expect(menu.classList.contains("open")).toBeFalsy();
+    act(() => {
+      menu.click();
+    })
+    const links = screen.getAllByRole("listitem");
+    const anchors = screen.getAllByRole("link")
 
     expect(links.length).toBe(10);
     expect(links[0].textContent).toBe("description.perfilsocial");
     expect(links[1].textContent).toBe("description.herramientajuego");
     expect(links[2].textContent).toBe("description.herramientaruta");
     expect(links[3].textContent).toBe("-");
-    expect(links[4].querySelector("div").getAttribute("href")).toBe("https://www.aecar.org/modalidades.php?tipo=crawler");
-    expect(links[5].querySelector("div").getAttribute("href")).toBe("https://www.clubzonarc.es/");
-    expect(links[6].querySelector("div").getAttribute("href")).toBe("https://isrcc.eu/");
+    expect(anchors[0].getAttribute("href")).toBe("https://www.aecar.org/modalidades.php?tipo=crawler");
+    expect(anchors[1].getAttribute("href")).toBe("https://www.clubzonarc.es/");
+    expect(anchors[2].getAttribute("href")).toBe("https://isrcc.eu/");
     expect(links[7].textContent).toBe("-");
     expect(links[8].textContent).toBe("description.politicaprivacidad");
     expect(links[9].textContent).toBe("description.aboutus");
