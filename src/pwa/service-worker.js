3966ab109c7e31c5308bd66b12720c05d8147efe
neuroflip <jaanguita@gmail.com>
2024-11-01 23:54:50 +0100

    Avoid to refresh games reexecutes (#243)
    * Avoid to refresh games reexecutes
    ---------
    
    Co-authored-by: neuroflip <neuroflip@gmail.com>

--- src/pwa/service-worker.js
+++ src/pwa/service-worker.js
@@ -4,8 +4,8 @@ import { StaleWhileRevalidate } from "workbox-strategies"
 import { setCacheNameDetails } from 'workbox-core'
 
 const CACHE_NAME = 'crawlearcom'
-const CACHE_VERSION = '1.0.31'
-const PREVIOUS_CACHE_VERSION = '1.0.30'
+const CACHE_VERSION = '1.0.32'
+const PREVIOUS_CACHE_VERSION = '1.0.31'
 //crawlearcom-crawlearcom-precache-1.0.21
 
 setCacheNameDetails({
