7859572da57d7a619bd7ee08787edc95ed9ecf63
neuroflip <jaanguita@gmail.com>
2024-11-12 10:51:31 +0100

    Changed the routes creation form for a better usability and error man… (#253)
    * Changed the routes creation form for a better usability and error management
    
    * More error control and form organization
    
    ---------
    
    Co-authored-by: neuroflip <jaanguita@MacBook-Pro-de-Jose.local>
    Co-authored-by: neuroflip <neuroflip@gmail.com>

--- src/modules/routesManagement/components/RoutesConfigurator.tsx
+++ src/modules/routesManagement/components/RoutesConfigurator.tsx
@@ -4,6 +4,7 @@ import ErrorBox from '../../../components/ErrorBox'
 import UseRoutesConfigurator from '../hooks/UseRoutesConfigurator'
 import { useTranslation } from 'react-i18next'
 import { GpxRouteMap } from 'react-gpxroutemap'
+import Utils from '../../../Utils'
 
 
 import '../styles/RoutesConfigurator.scss'
@@ -15,32 +16,53 @@ interface RoutesConfiguratorProps {
 }
 
 function RoutesConfigurator({ inRoute, onRouteCreated, onBackClick }: RoutesConfiguratorProps) {
-    const [route, error, onCreateRoute, onDificultyChange, onInputChange, onFileResolved] = UseRoutesConfigurator(inRoute, onRouteCreated)
+    const [route, error, onCreateRoute, onDificultyChange, onInputChange, onFileResolved, validateFormVata] = UseRoutesConfigurator(inRoute, onRouteCreated)
     const { t } = useTranslation(['main'])
 
     return <>
         <div className='headerText bold sectionTitle'>{t('description.seccionderutas')}</div>
         <div className="routesManagement routesManagementCreation rounded rounded2">
-            <ErrorBox message={error}></ErrorBox>
-
             <div className="routeMap center">
-                <label>{t('description.mapaderuta')}</label>
+                <label htmlFor='gpxData' className="formRequiredValue">{t('description.mapaderuta')}</label>
+                { !route.gpx.data && <div className='formError'>{ t('error.noruta')} </div>}
+                <input minLength={1} id="gpxData" type="hidden" value={route.gpx.data} />
                 <GpxRouteMap gpx={route.gpx.data} onFileResolved={onFileResolved} onRouteRecorded={onFileResolved}></GpxRouteMap>
             </div>
 
-            <div className="routeName center">{t('description.nombrederuta')} <input name='name' value={route.name} onChange={(value) => {
-                onInputChange('name', value)
-            }}></input></div>
-            <div className="routeDescription center">{t('description.descripcion')} <textarea name='description' value={route.description} onChange={(value) => {
-                onInputChange('description', value)
-            }}></textarea></div>
-            <div className="routeIsPublic center">{t('description.publico')} <input type="checkbox" defaultChecked={route.isPublic} name='isPublic' onChange={(value) => {
-                onInputChange('isPublic', value)
-            }}></input></div>
-            <div className="routeScale center">{t('description.escala')} <input name='scale' value={route.scale} onChange={(value) => {
-                onInputChange('scale', value)
-            }}></input></div>
-            <div className="routeDifficulty center">{t('description.dificultad')} <select onChange={onDificultyChange} value={route.dificulty}>
+            <div className="routeName center">
+                <label htmlFor='routeName' className="formRequiredValue">{t('description.nombrederuta')}</label>
+                { !route.name && <div className='formError'>{ t('error.nonombre')} </div>}
+                <input minLength={1} id="routeName" name='name' value={route.name} onChange={(value) => {
+                    onInputChange('name', value)
+                }}></input>
+            </div>
+
+            <div className="routeDescription center">
+                <label htmlFor="routeDescription" className="formRequiredValue">{t('description.descripcion')}</label>
+                { !route.description && <div className='formError'>{ t('error.nodescripcion')} </div>}
+                <textarea minLength={1} id="routeDescription" name='description' value={route.description} onChange={(value) => {
+                    onInputChange('description', value)
+                }}></textarea>
+            </div>
+
+            <div className="routeIsPublic center">
+                <label htmlFor='routeIsPublic'>{t('description.publico')}</label>
+                <input id="routeIsPublic" type="checkbox" defaultChecked={route.isPublic} name='isPublic' onChange={(value) => {
+                    onInputChange('isPublic', value)
+                }}></input>
+            </div>
+
+            <div className="routeScale center">
+                <label htmlFor='routeScale' className="formRequiredValue">{t('description.escala')}</label>
+                { !route.scale && <div className='formError'>{ t('error.noescala')} </div>}
+                <input minLength={1} id="routeScale" name='scale' value={route.scale} onChange={(value) => {
+                    onInputChange('scale', value)
+                }}></input>
+            </div>
+
+            <div className="routeDifficulty center">
+                <label htmlFor='routeDifficult'>{t('description.dificultad')}</label>
+                <select id="routeDifficult" onChange={onDificultyChange} value={route.dificulty}>
                 <option value="1">1</option>
                 <option value="2">2</option>
                 <option value="3">3</option>
@@ -48,15 +70,28 @@ function RoutesConfigurator({ inRoute, onRouteCreated, onBackClick }: RoutesConf
                 <option value="5">5</option>
             </select>
             </div>
-            <div className="youtubeVideo center">{t('description.video')}<input name='youtubeVideo' value={route.youtubeVideo} onChange={(value) => {
-                onInputChange('youtubeVideo', value)
-            }}></input></div>
-            <div className="routePoint center">{t('description.puntoencuentro')}<input name='locationMapUrl' value={route.locationMapUrl} onChange={(value) => {
+
+            <div className="youtubeVideo center">
+                <label  htmlFor='youtubeVideo'>{t('description.video')}</label>
+                { route.youtubeVideo && !Utils.isYoutubeUrl(route.youtubeVideo) && <div className='formError'>{ t('error.invalidyoutube')} </div>}
+                <input id="youtubeVideo" name='youtubeVideo' value={route.youtubeVideo} onChange={(value) => {
+                    onInputChange('youtubeVideo', value)
+                }}></input>
+            </div>
+
+            <div className="routePoint center">
+                <label htmlFor='routeLocation' className="formRequiredValue">{t('description.puntoencuentro')}</label>
+                { !route.locationMapUrl && <div className='formError'>{ t('error.nolocalizacion')} </div>}
+                { route.locationMapUrl && !Utils.isGoogleMapsUrl(route.locationMapUrl) && <div className='formError'>{ t('error.invalidmaps')} </div>}
+                <input minLength={1} id="routeLocation" name='locationMapUrl' value={route.locationMapUrl} onChange={(value) => {
                 onInputChange('locationMapUrl', value)
             }}></input></div>
 
+            <ErrorBox message={error}></ErrorBox>
             <div className="endButtons">
-                <button className='importantNote' onClick={onCreateRoute}>{route.rid ? t('description.modificar') : t('description.crear')}</button>
+                <button className='importantNote' onClick={onCreateRoute} disabled={!validateFormVata()}>
+                    {route.rid ? t('description.modificar') : t('description.crear')}
+                </button>
                 <button onClick={onBackClick}>{ t('description.atras')}</button>
             </div>
         </div>
--- src/modules/routesManagement/hooks/UseRoutesConfigurator.ts
+++ src/modules/routesManagement/hooks/UseRoutesConfigurator.ts
@@ -1,6 +1,7 @@
 import * as React from 'react'
 import Route, { GeoPoint } from '../Route'
 import { useTranslation } from 'react-i18next'
+import Utils from '../../../Utils'
 
 const LAT_DIVISOR = 36
 const LON_DIVISOR = 72
@@ -14,16 +15,8 @@ function UseRoutesConfigurator(inRoute: Route, onRouteCreated: Function): Array<
     function onCreateRoute() {
         window.scrollTo(0,0)
 
-        if (route.name.length <= 0) {
-            setError(t('error.nonombre'))
-        } else if (route.description.length <= 0) {
-            setError(t('error.nodescripcion'))
-        } else if (route.scale.length <= 0) {
-            setError(t('error.noescala'))
-        } else if (route.locationMapUrl.length <= 0) {
-            setError(t('error.nolocalizacion'))
-        } else if (route.gpx.data.length <= 0) {
-            setError(t('error.noruta'))
+        if (!validateFormState()) {
+            setError(t('error.nodatosrequired'))
         } else {
             setError('')
             fb.setRoute(route, (route: Route) => {}, () => {})
@@ -95,7 +88,13 @@ function UseRoutesConfigurator(inRoute: Route, onRouteCreated: Function): Array<
         })
     }
 
-    return [route, error, onCreateRoute, onDificultyChange, onInputChange, onFileResolved]
+    function validateFormState() {
+        return (route.name.length > 0) && (route.description.length > 0) &&
+            (route.scale.length > 0) && (route.locationMapUrl.length > 0) &&
+            (route.gpx.data.length > 0) && (route.youtubeVideo ? Utils.isYoutubeUrl(route.youtubeVideo) : true)
+    }
+
+    return [route, error, onCreateRoute, onDificultyChange, onInputChange, onFileResolved, validateFormState]
 }
 
 export default UseRoutesConfigurator
