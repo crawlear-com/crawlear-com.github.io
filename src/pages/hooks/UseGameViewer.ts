72a9369c12b37479d05a4a80358aab2d12ba74a6
neuroflip <jaanguita@gmail.com>
2024-12-03 00:42:47 +0100

    getGame at Firenase moved to base controller to be called offline at gameViewer (#261)
    Co-authored-by: neuroflip <neuroflip@gmail.com>

--- src/pages/hooks/UseGameViewer.ts
+++ src/pages/hooks/UseGameViewer.ts
@@ -3,17 +3,17 @@ import { Game } from '../../games/Game'
 import Analytics from '../../Analytics'
 
 function UseGameViewer(gid: string) {
-    const firebase = window.crawlear.fb
+    const fbBase = window.crawlear.fbBase
     const [game, setGame] = React.useState<any>({})
 
     React.useEffect(() => {
         Analytics.pageview(`/gameviewer?gid=${gid}`)
-        gid && gid.length > 0 && firebase.getGame(gid, (newGame: Game) => {
+        gid && gid.length > 0 && fbBase.getGame(gid, (newGame: Game) => {
             if (newGame.isPublic) {
                 setGame(newGame)
             }
         })
-    }, [firebase, gid])
+    }, [fbBase, gid])
 
     return [game]
 }
