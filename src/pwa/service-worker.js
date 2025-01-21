bc66888e3e6faa660028ccaea2b3eafec37f10ed
neuroflip <jaanguita@gmail.com>
2025-01-21 11:40:27 +0100

    Update service-worker.js version (#285)

--- src/pwa/service-worker.js
+++ src/pwa/service-worker.js
@@ -4,7 +4,7 @@ import { StaleWhileRevalidate } from "workbox-strategies"
 import { setCacheNameDetails } from 'workbox-core'
 
 const CACHE_NAME = 'crawlearcom'
-const CACHE_VERSION = '1.0.40'
+const CACHE_VERSION = '1.0.41'
 //const PREVIOUS_CACHE_VERSION = '1.0.34'
 //crawlearcom-crawlearcom-precache-1.0.21
 
